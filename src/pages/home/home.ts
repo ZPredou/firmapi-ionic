18400fa9c4fb8125681861ec2e7e8e831224025f
Decourty William <williamdecourty@live.fr>
2018-03-14 11:58:52 +0100

    number of companies & list updating & resources for deploy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,37 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
-import {DetailCompanyPage} from '../detail-company/detail-company';
-import {FirmApiProvider} from '../../providers/firm-api/firm-api';
-import {CompanyInterface} from '../../providers/firm-api/firm-api-interface';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { DetailCompanyPage } from '../detail-company/detail-company';
+import { FirmApiProvider } from '../../providers/firm-api/firm-api';
+import { CompanyInterface } from '../../providers/firm-api/firm-api-interface';
+import { UrlService } from '../../providers/url';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
 })
 export class HomePage {
+  subscription: Subscription;
   items: CompanyInterface[] = [];
   nhits: number;
+  params = '';
 
-  constructor(public navCtrl: NavController, public firmApiProvider: FirmApiProvider) {
-    this.firmApiProvider.searchCompanies('', 30).subscribe(data => {
+  constructor(public navCtrl: NavController, public firmApiProvider: FirmApiProvider, private sendUrlService: UrlService) {
+    this.firmApiProvider.searchCompanies(this.params, 30).subscribe(data => {
       this.nhits = data.nhits;
-      for (let i=0; i < data.records.length; i++) {
-        console.log(data.records[i]);
+      for (let i = 0; i < data.records.length; i++) {
         this.items.push(data.records[i]);
       }
+    });
+    this.subscription = this.sendUrlService.getUrl().subscribe(url => {
+      this.items = [];
+      this.params = url;
+      this.firmApiProvider.searchCompanies(this.params, 30).subscribe(data => {
+        this.nhits = data.nhits;
+        for (let i = 0; i < data.records.length; i++) {
+          this.items.push(data.records[i]);
+        }
+      });
     });
   }
 
@@ -31,7 +44,6 @@ export class HomePage {
     setTimeout(() => {
       this.firmApiProvider.searchCompanies('', 30, this.items.length).subscribe(data => {
         for (let i = 0; i < data.records.length; i++) {
-          console.log(data.records[i]);
           this.items.push(data.records[i]);
           infiniteScroll.complete();
         }
