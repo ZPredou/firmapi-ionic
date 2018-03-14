79ac09e1d0ada5d05e8715562c3c5b93b2341489
Decourty William <williamdecourty@live.fr>
2018-03-14 12:23:59 +0100

    map updates with ( & without ) filters 👏

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Subscription } from 'rxjs/Subscription';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { UrlService } from '../../providers/url';
 
 @Component({
   selector: 'page-map',
@@ -7,8 +9,18 @@ import { NavController } from 'ionic-angular';
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController) {
+  subscription: Subscription;
+  mapUrl: SafeResourceUrl;
 
+  constructor(private sendUrlService: UrlService, private sanitizer: DomSanitizer) {
+    this.subscription = this.sendUrlService.getUrl().subscribe(data => {
+      this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        'https://public.opendatasoft.com/explore/embed/dataset/sirene/map/?q=' + data);
+    });
+    if(!this.mapUrl)
+    {
+      this.mapUrl= this.sanitizer.bypassSecurityTrustResourceUrl(
+        'https://public.opendatasoft.com/explore/embed/dataset/sirene/map/');
+    }
   }
-
 }
